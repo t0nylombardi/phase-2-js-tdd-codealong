03ae053f692e0d4f455fcc106ac2aeffe7dace29
t0nylombardi <alombardi.331@gmail.com>
2024-06-03 13:20:21 -0400

    added test

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 // Your code here
+export const currentAgeForBirthYear = (birthYear) => {
+  console.log("birthYear: ", birthYear);
+  console.log("new Date().getFullYear(): ", new Date().getFullYear());
+  return new Date().getFullYear() - birthYear;
+};
+
+const year = 1984;
+console.log(currentAgeForBirthYear(year)); // 38
